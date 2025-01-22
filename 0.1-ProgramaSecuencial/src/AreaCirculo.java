b54e6d7ce2973a557c33b064cdf8e46be8e58e38
Samuel Salinas Laguna <samuel08@gmail.com>
2025-01-22 11:57:15 -0600

    Logro: Primeros Programas Secuenciales

---
+++ 0.1-ProgramaSecuencial/src/AreaCirculo.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+        double PI = 3.1416;
+        double a = 0;
+        double r = 0;
+        String salida;
+
+        r = Double.parseDouble(JOptionPane.showInputDialog("Ingresar el valor de r: "));
+        a = PI * Math.pow(r,2);
+
+        JOptionPane.showMessageDialog(null, "El resultado de la expresion es: " + a);
+    }
+}
